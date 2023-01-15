c4f13d44054b563bba1eaf01492d3b14efa0fe22
alexdevilmaycare <sashalevk111aaa@gmail.com>
2023-01-15 14:58:54 +0300

    Experimented with prettier

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import _ from "lodash"
+// import _ from "lodash";
 
-const intersection = _.intersection ([1, 2, 3], [3, 4, 5]); 
-console.log(intersection)
+// const intersection = _.intersection([1, 2, 3], [3, 4, 5]);
+// console.log(intersection);
+
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
